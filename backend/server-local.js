93bd57832551a6b99af890f5ee60ea8edd9d5e94
johnatan-boot <johnatan.quenes@gmail.com>
2025-07-12 17:48:22 -0300

    testes

--- backend/server-local.js
+++ backend/server-local.js
@@ -1,6 +1,7 @@
-const app = require("./api/server.js");
+/*const app = require("./api/server.js");
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando localmente em http://localhost:${PORT}`);
 });
+*/
